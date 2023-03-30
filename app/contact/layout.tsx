805a8b2b15335826aa6737cab241ca098e1c613b
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-03-30 11:33:04 +0530

    Add files via upload

---
+++ app/contact/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ContactLayout({
+    children,
+}:{
+    children: React.ReactNode
+})
+{
+    return(
+        <React.Fragment>
+          { children}
+        </React.Fragment>
+    );
+}
