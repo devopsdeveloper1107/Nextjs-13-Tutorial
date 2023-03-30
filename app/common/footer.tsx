b36b43b2b8ae7fb50a522bd65ccdac8788ecb166
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-03-30 11:29:26 +0530

    Add files via upload

---
+++ app/common/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer()
+{
+    return(
+        <React.Fragment>
+            <footer>
+                <h2>Footer Section</h2>
+            </footer>
+        </React.Fragment>
+    );
+}
---
+++ app/common/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header()
+{
+    return(
+        <React.Fragment>
+            <nav>
+                <ul>
+                    <li>Home </li>
+                    <li>About Us </li>
+                    <li>Contact Us </li>
+                </ul>
+            </nav>
+        </React.Fragment>
+    );
+}
