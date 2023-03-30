2e646ee36d6ec071eecbd8be3621cd4686dd8558
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-03-30 11:30:59 +0530

    Add files via upload

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+export default function About()
+{
+    return(
+        <React.Fragment>
+            <h2>About Page</h2>
+            <p>Hello , it is about us page</p>
+        </React.Fragment>
+    );
+}
