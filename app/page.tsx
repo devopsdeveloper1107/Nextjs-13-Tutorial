06cfd24f89ce1d553d7c2907346f48004ed674b9
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-04-10 10:20:15 +0530

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+import React from 'react';
+import Banner from './common/banner';
 
 export default function Home() {
   return (
-    <main>
-     <h1>Welcome to Next Js Tutorial</h1>
-    </main>
+    <React.Fragment> 
+        <Banner/>
+    <section>     
+      <div className='container'>
+       <h1>Welcome to Next Js Tutorial</h1>
+       <p>
+       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+       </p>
+      </div>     
+    </section>
+    </React.Fragment>
   )
 }
